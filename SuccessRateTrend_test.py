2201ccd70e9853cb5bdbbfc6e12502ff457639f4
PEIJUN WANG <40076451+PEIJUNWANG2231@users.noreply.github.com>
2019-03-17 14:07:44 +1100

    Add files via upload

--- SuccessRateTrend_test.py
+++ SuccessRateTrend_test.py
@@ -7,12 +7,25 @@ This is a temporary script file.
 
 # to gain the success rate  of trading system
 import pandas as pd
+import datetime
+
+def get_month_range(start_day,end_day):
+  months = (end_day.year - start_day.year)*12 + end_day.month - start_day.month
+  month_range = ['%s-%s'%(start_day.year + mon//12,mon%12+1) 
+                    for mon in range(start_day.month-1,start_day.month + months)]
+  list2=month_range
+  
+  for i in range(len(list2)):
+        if len(list2[i])!=7:
+            list2[i]=list2[i][:5]+'0'+list2[i][5:]
+  
+  return list2
 
 
 def new_column(file_name,m):
-    path=r"D:\Python Codes Lib\ASX transcript"
+    path="D:/Python Codes Lib/ASX transcript"
     
-    tr = '\\'
+    tr = '/'
     
     df=pd.read_csv(path+tr+file_name+'.csv')
 
@@ -29,21 +42,24 @@ def new_column(file_name,m):
     for i in range(len(df)):
         df['Year&Month'][i]=df['Date'][i][:7]
     
-    list1=df['Year&Month'].tolist()
+    #引入日期函数
+    last_day=str(df['Date'][len(df)-1:])
     
-    list2=[]
-    for i in range(len(list1)-1):
-        if list1[i]!=list1[i+1]:
-            list2.append(list1[i])
+    year=int(last_day[8:12])
+    month=int(last_day[13:15])
+
+    list2=get_month_range(datetime.date(2000, 1, 1),datetime.date(year,month,1))
     
     #gain round number
     k=(len(list2)//m)*m
     
     d={}
     
+    #先填充list2的元素
     for i in range(0,k-m):
         d[list2[i]]=list2[i]
     
+    #类聚，m个为一组
     for i in range(0,k-m,m):
         for v in range(1,m):
             d[list2[i+v]]=list2[i]
@@ -52,12 +68,15 @@ def new_column(file_name,m):
     
     list4=list(d.keys())
     
+    #调整df 长度
+    #到list4的最后一个元素
     target=list2[list2.index(list4[-1])+1]
     
     target_index=list3.index(target)
     
     df=df[:target_index][:]
     
+    #用字典的方式嵌套进去
     df['New_Year&Month']=0
     
     for i in range(len(df)):
@@ -68,16 +87,17 @@ def new_column(file_name,m):
     
     
     df1=df.groupby(['New_Year&Month']).mean()
+    
+    df1=df1.loc[:, ['target']]
+    
     path='D:/Python Codes Lib/'+file_name+'_month'+str(m)+'_SuccessRate.csv'
     df1.to_csv(path)
     return df1
 
 def main():
-    new_column('5_8_m=1',m=12)
-    new_column('5_8_13_m=1',m=12)
-    new_column('8_13_m=1',m=12)
+    new_column('5_8_m=1',m=4)
+    new_column('5_8_13_m=1',m=4)
+    new_column('8_13_m=1',m=4)
     
 if __name__ == '__main__':
     main()
-
-
